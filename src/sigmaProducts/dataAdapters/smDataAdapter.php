fe4a4d162882bb471af050ca511097964fd8f4c4
vvetrov <57356786+cudev-vvetrov@users.noreply.github.com>
2020-06-20 00:38:33 +0300

    reformat code style to PSR-2

--- src/sigmaProducts/dataAdapters/smDataAdapter.php
+++ src/sigmaProducts/dataAdapters/smDataAdapter.php
@@ -4,28 +4,30 @@
 namespace sigmaProducts\dataAdapters;
 
 
+use Exception;
+
 /** interface for product data storage manipulation
  * @author Valery Vetrov
  */
 interface smDataAdapter
 {
-	/**
-	 * Sets item prices
-	 *
-	 * @param string $productName - name of the product
-	 * @param float|array $prices - float price for one item or array like [qty=>price...]
-	 * @param bool $reset - if true then clear previously set prices
-	 * @return self
-	 */
-	public function setItem($productName, $prices, $reset = false);
+    /**
+     * Sets item prices
+     *
+     * @param string $productName - name of the product
+     * @param float|array $prices - float price for one item or array like [qty=>price...]
+     * @param bool $reset - if true then clear previously set prices
+     * @return self
+     */
+    public function setItem($productName, $prices, $reset = false);
 
-	/**
-	 * Gets item prices as array like [qty=>price...]
-	 * MUST return array with qty's in reverse order
-	 *
-	 * @param string $productName
-	 * @return array
-	 * @throws \Exception - if product does not exist
-	 */
-	public function getItem($productName);
+    /**
+     * Gets item prices as array like [qty=>price...]
+     * MUST return array with qty's in reverse order
+     *
+     * @param string $productName
+     * @return array
+     * @throws Exception - if product does not exist
+     */
+    public function getItem($productName);
 }
--- src/sigmaProducts/productCalcTerminal.php
+++ src/sigmaProducts/productCalcTerminal.php
@@ -5,41 +5,48 @@ namespace sigmaProducts;
 
 use sigmaProducts\dataAdapters\smDataAdapter;
 use sigmaProducts\dataAdapters\smTextData;
+
 /*
  * wrapper for productCalc class to make terminal as required in the task
  * */
-class productCalcTerminal {
-	private $adapter;
-	private $calc;
 
-	/** Builds the terminal instance
-	 * @param smDataAdapter $adapter
-	 */
-	public function __construct($adapter) {
-		// will use adapter for setting new prices
-			$this->adapter = $adapter;
-		$this->calc = new productCalc($this->adapter);
-	}
+class productCalcTerminal
+{
+    private $adapter;
+    private $calc;
 
-	/** Sets the price for product
-	 *
-	 * @param string $productName
-	 * @param int $amount
-	 * @param float $price
-	 */
-	public function setPricing($productName, $amount, $price) {
-		$this->adapter->setItem($productName,[$amount=>$price]);
-	}
+    /** Builds the terminal instance
+     * @param smDataAdapter $adapter
+     */
+    public function __construct($adapter)
+    {
+        // will use adapter for setting new prices
+        $this->adapter = $adapter;
+        $this->calc = new productCalc($this->adapter);
+    }
 
-	/** Adds the product to the pool
-	 * @param string $productName
-	 */
-	public function scanItem($productName) {
-		$this->calc->addProduct($productName);
-	}
+    /** Sets the price for product
+     *
+     * @param string $productName
+     * @param int $amount
+     * @param float $price
+     */
+    public function setPricing($productName, $amount, $price)
+    {
+        $this->adapter->setItem($productName, [$amount => $price]);
+    }
 
-	/** Calculates the total for the pool */
-	public function getTotal() {
-		return$this->calc->getTotal();
-	}
+    /** Adds the product to the pool
+     * @param string $productName
+     */
+    public function scanItem($productName)
+    {
+        $this->calc->addProduct($productName);
+    }
+
+    /** Calculates the total for the pool */
+    public function getTotal()
+    {
+        return $this->calc->getTotal();
+    }
 }
